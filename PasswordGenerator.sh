dbd1773df056a94c3f5d896fc2a9730d1fb91955
cu-sp <down1995@tutanota.com>
2024-02-16 17:17:48 -0500

    added passwordgenerator.sh file

---
+++ PasswordGenerator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#Password Generator
+#Using base64
+#
+#Dylan Dodds
+#Feb. 16 2024
+#
+
+echo "This is a simple password generator"
+echo "Enter desired length of password: "
+read PASS_LENGTH
+echo "Number of passwords: "
+read PASS_NUMBER
+
+for p in $(seq 1 $PASS_NUMBER); do
+	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
+done
